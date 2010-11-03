350746e65c629586b6f6952eef2552a92a59baed
Darius Bacon <darius@wry.me>
2010-11-02 22:59:14 -0700

    a meta-interpreter that can run terp.pl, to get started

---
+++ prolog/meta.pl
@@ -0,0 +1,17 @@
+% -*- Prolog -*-
+% Prolog meta-interpreter. There are better ones, but this is mine.
+% Just good enough to run terp.pl if you enter
+%    source.
+% at the prompt before loading it (so that clause(G, Antecedent)
+% can find the program's clauses).
+
+meta(true).
+meta((G1, G2)) :- meta(G1), meta(G2).
+meta(G) :- special(G), call(G).
+meta(G) :- \+ special(G), clause(G, Antecedent), meta(Antecedent).
+
+special((_, _)).
+special(_ is _).
+special(_ =\= _).
+special(write(_)).
+special(nl).
