c7f09ebabd371552d65aa5f39d9e986768fd0fb9
Darius Bacon <darius@wry.me>
2010-11-03 18:46:33 -0700

    toy partial evaluator with trace pruner

---
+++ prolog/pe.pl
@@ -0,0 +1,47 @@
+% -*- Prolog -*-
+% Toy partial evaluator.
+% Enter
+%    source.
+% at the prompt before loading terp.pl (so that clause(G, Antecedent)
+% can find the program's clauses).
+
+% G for goal
+% R for residual
+% SR for simplified residual
+
+peval(G, SR) :-
+        pe(G, R),
+        simplify(R, SR).
+
+pe((G1, G2), (R1, R2)) :-
+        !,
+        pe(G1, R1),
+        pe(G2, R2).
+pe(G, basic(G)) :-
+        basic(G), !,
+        call(G).
+pe(G, R) :-
+        clause(G, Antecedent),
+        pe(Antecedent, R).
+
+basic(G) :-
+        predicate_property(G, built_in).
+
+simplify((R1, R2), SR) :-
+        simplify(R1, SR1),
+        simplify(R2, SR2),
+        append(SR1, SR2, SR).
+simplify(basic(G), []) :- trivial(G).
+simplify(basic(G), [G]) :- \+ trivial(G).
+
+trivial(true).
+trivial(X is E) :-
+        ground(X),
+        ground(E).
+trivial(T1 =\= T2) :-
+        ground(T1),
+        ground(T2).
+
+append([], L, L).
+append([H|T], L, [H|Z]) :-
+        append(T, L, Z).
